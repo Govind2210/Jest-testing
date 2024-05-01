4482104ae5988b5859c6dfa60520758e376016ce
Trushant <trushant@setoo.co>
2024-05-01 15:37:34 +0530

    input fields test case

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>First test case!</h1>
-     <img title='Google image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/640px-Google_Images_2015_logo.svg.png'/>
+      <h1>Input test case!</h1>
+      <input type="text" placeholder="Enter the Name" name="userName" id="userId" />
+      <br />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,17 @@
 import { render , screen } from "@testing-library/react";
 import App from "./App";
 
-test("testing the first case" ,()=>{
+/**
+ * To have Attribute is used for checking the Attribute in the given Tags
+ */
+
+test("testing the input field",()=>{
   render(<App/>); // import app
-  // const test = screen.getByText("First Test Case!"); // fetch the text from the render  & this will check with case-senstive
-  const test = screen.getByText(/First Test Case!/i); // fetch the text from the render  & This will Not  Check With Case-Senstive
-  const title = screen.getByTitle("Google image");
-  expect(test).toBeInTheDocument(); // we can have miltuple expect;
-  expect(title).toBeInTheDocument();
+  const input = screen.getByRole('textbox'); // textbox is use to check is there any input fields is there or not.
+  const inputPlaceholder = screen.getByPlaceholderText("Enter the Name");
+  expect(input).toBeInTheDocument(); // using this for attribute below 
+  expect(inputPlaceholder).toBeInTheDocument();  // checking placeholder
+  // checking the attribute
+  expect(inputPlaceholder).toHaveAttribute("name","userName") // checking the attribute
+  expect(inputPlaceholder).toHaveAttribute("id","userId") // chcking the ID
 })
