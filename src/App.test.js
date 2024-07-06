5eae622f1516aaf8cdb90cfb98ed3e111752cae1
govind2020 <govind@setoo.co>
2024-07-06 15:09:07 +0530

    feat(describe): learn the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,62 @@
-import { render , screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 /**
- * Press a to run all tests.
- › Press f to quit "only failed tests" mode.
- › Press o to only run tests related to changed files.
- › Press q to quit watch mode.
- › Press p to filter by a filename regex pattern.
- › Press t to filter by a test name regex pattern.
- › Press Enter to trigger a test run.
+ * SO here i have learn
+ * only:- SO if we use only on any descriobe :- it will test that describe or the test function
+ * skip :- if you dont want to test that function and add only to other function it will will skipped that test
+ * Nested:- so we can describe the  inside the describe function or any test cases
  */
 
-
-
-test("testing the input field",()=>{
-  render(<App/>); // import app
-  const input = screen.getByRole('textbox'); // textbox is use to check is there any input fields is there or not.
+test("testing the input field", () => {
+  render(<App />);
+  const input = screen.getByRole("textbox"); // textbox is use to check is there any input fields is there or not.
   const inputPlaceholder = screen.getByPlaceholderText("Enter the Name");
-  expect(input).toBeInTheDocument(); // using this for attribute below 
-  expect(inputPlaceholder).toBeInTheDocument();  // checking placeholder
+  expect(input).toBeInTheDocument(); // using this for attribute below
+  expect(inputPlaceholder).toBeInTheDocument(); // checking placeholder
   // checking the attribute
-  expect(inputPlaceholder).toHaveAttribute("name","userName") // checking the attribute
-  expect(inputPlaceholder).toHaveAttribute("id","userId") // chcking the ID
-})
+  expect(inputPlaceholder).toHaveAttribute("name", "userName"); // checking the attribute
+  expect(inputPlaceholder).toHaveAttribute("id", "userId"); // chcking the ID
+});
+
+describe.only("Testing the Group describe for UI", () => {
+  // only is used for for selecting the testing if it is not only they will be skipped
+  test("test case 1", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("test case 2", () => {
+    render(<App />);
+  });
+
+  test("test case 3", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+  });
+});
+
+describe.only("API Test Case", () => {
+  test("API test case 1", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("API test case 2", () => {
+    render(<App />);
+  });
+
+  test("API test case 3", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+  });
+
+  describe("innser describe test case",()=>{
+    test("Inner test case for API test case 1", () => {
+      render(<App />);
+      const input = screen.getByRole("textbox");
+    });
+  })
+});
