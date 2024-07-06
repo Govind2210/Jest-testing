3fa100ee2c05ff87adb5ae4fc61dc70771c3949b
govind2020 <govind@setoo.co>
2024-07-06 15:44:24 +0530

    add test event on chnage and on click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState("");
+  const [click, setClick] = useState("");
+  console.log("data==>", data);
   return (
-    <div className="App">
-      <h1>Input test case!</h1>
-      <input type="text" placeholder="Enter the Name" name="userName" id="userId" />
-      <br />
-    </div>
+    <>
+      <div className="App">
+        <h1>Input test onChange case!</h1>
+        <input
+          type="text"
+          value={data}
+          placeholder="Enter the Name"
+          name="userName"
+          id="userId"
+          onChange={(e) => {
+            setData(e.target.value);
+          }}
+        />
+        <br />
+      </div>
+      <div className="App">
+        <h1>Input test onclick case! withn button</h1>
+        <button
+          onClick={() => {
+            setClick("UPDATED DATA!!!!");
+          }}
+        >
+          Update Data
+        </button>
+        <h1>{click}</h1>
+      </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,62 +1,18 @@
-import { render, screen } from "@testing-library/react";
+/* eslint-disable jest/valid-expect */
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-/**
- * SO here i have learn
- * only:- SO if we use only on any descriobe :- it will test that describe or the test function
- * skip :- if you dont want to test that function and add only to other function it will will skipped that test
- * Nested:- so we can describe the  inside the describe function or any test cases
- */
-
-test("testing the input field", () => {
+// on change testing function
+test("change event testing", () => {
   render(<App />);
-  const input = screen.getByRole("textbox"); // textbox is use to check is there any input fields is there or not.
-  const inputPlaceholder = screen.getByPlaceholderText("Enter the Name");
-  expect(input).toBeInTheDocument(); // using this for attribute below
-  expect(inputPlaceholder).toBeInTheDocument(); // checking placeholder
-  // checking the attribute
-  expect(inputPlaceholder).toHaveAttribute("name", "userName"); // checking the attribute
-  expect(inputPlaceholder).toHaveAttribute("id", "userId"); // chcking the ID
+  let inputField = screen.getByRole("textbox");
+  fireEvent.change(inputField, { traget: { value: "abc" } });
+  expect(inputField.value).toBe("abctest");
 });
 
-describe.only("Testing the Group describe for UI", () => {
-  // only is used for for selecting the testing if it is not only they will be skipped
-  test("test case 1", () => {
-    render(<App />);
-    const input = screen.getByRole("textbox");
-    expect(input).toBeInTheDocument();
-  });
-
-  test("test case 2", () => {
-    render(<App />);
-  });
-
-  test("test case 3", () => {
-    render(<App />);
-    const input = screen.getByRole("textbox");
-  });
-});
-
-describe.only("API Test Case", () => {
-  test("API test case 1", () => {
-    render(<App />);
-    const input = screen.getByRole("textbox");
-    expect(input).toBeInTheDocument();
-  });
-
-  test("API test case 2", () => {
-    render(<App />);
-  });
-
-  test("API test case 3", () => {
-    render(<App />);
-    const input = screen.getByRole("textbox");
-  });
-
-  describe("innser describe test case",()=>{
-    test("Inner test case for API test case 1", () => {
-      render(<App />);
-      const input = screen.getByRole("textbox");
-    });
-  })
+test("on click event test", () => {
+  render(<App />);
+  let btn = screen.getByRole("button");
+  fireEvent.click(btn);
+  expect(screen.getByText("UPDATED DATA!!!!")).toBeInTheDocument();
 });
