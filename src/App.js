b020e026680d3a87070a621d0323c1b0dfae5a19
govind2020 <govind@setoo.co>
2024-07-07 12:47:42 +0530

    learn(placeholder): learn how to test multiple test holder

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>RTQ - getAllByLabelText</h1>
-        <label htmlFor="user-name">User Name:</label>{" "}
-        <input type="text" id="user-name" defaultValue={"gogo"} />
-        <br />
-        <br />
-        <label htmlFor="user-name1">User Name:</label>{" "}
-        <input type="text" id="user-name1" defaultValue={"gogo"} />
-        <br />
-        <br />
-        <label htmlFor="user-name2">User Name:</label>{" "}
-        <input type="text" id="user-name2" defaultValue={"gogo"} />
-      </div>
+        <h1>RTQ - getByPlaceholderText</h1>
+        <label htmlFor="user-name">User Name:</label>
+        <input type="text" id="user-name" placeholder="Enter User Name" />
+       </div>
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,8 @@
 import App from './App'
 const { screen, render } = require("@testing-library/react")
 
-test("Get All by Label text 1 test" , ()=>{
+test("Get placeHoler test" , ()=>{
   render(<App />);
-  const inputs = screen.getAllByLabelText('User Name:');
-  expect(inputs[0]).toBeInTheDocument()
-  expect(inputs[1]).toBeInTheDocument()
-  expect(inputs[2]).toBeInTheDocument()
-
-  for(let i = 0 ; i < inputs.length ; i++){
-    expect(inputs[i]).toHaveAttribute('type','text')
-    expect(inputs[i]).toBeInTheDocument()
-    expect(inputs[i]).toHaveValue("gogo")
-  }
-
+  const textPlaceHolder = screen.getByPlaceholderText("Enter User Name");
+  expect(textPlaceHolder).toBeInTheDocument();
 })
