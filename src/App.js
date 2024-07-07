e26c06651bd5d7f0f680d12932cc0d22ee5b76c1
govind2020 <govind@setoo.co>
2024-07-07 11:30:54 +0530

    fix - the tag name for the button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className="App">
         <h1>RTQ - get By Role!</h1>
         <input type="text"  defaultValue={"hello"} />
-        <nutton>Click Me</nutton>
+        <button>Click Me</button>
       </div>
     </>
   );
